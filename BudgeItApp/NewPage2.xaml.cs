2fb92287679469e979bf0abd1e1b909a39efa342
EveSans <106355002+EveSans@users.noreply.github.com>
2026-04-22 14:50:08 -0500

    Update NewPage2.xaml.cs

--- BudgeItApp/NewPage2.xaml.cs
+++ BudgeItApp/NewPage2.xaml.cs
@@ -1,13 +1,22 @@
+using BudgeItApp.ViewModels;
+
+
 namespace BudgeItApp;
 
 public partial class NewPage2 : ContentPage
 {
-	public NewPage2()
+    private readonly SplitViewModel _viewModel;
+
+
+    public NewPage2(SplitViewModel viewModel)
 	{
 		InitializeComponent();
-	}
+
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
     private async void GoPthree(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new NewPage3());
+        await Navigation.PushAsync(new NewPage3(_viewModel));
     }
 }
